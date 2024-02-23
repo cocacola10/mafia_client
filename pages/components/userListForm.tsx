587768ffd9309a2f64d8899db9d6b6b8b4a90c11
cocacola10 <hg9509198@gmail.com>
2024-02-23 15:33:59 +0900

    error occur - userListForm - userRole.role

--- pages/components/userListForm.tsx
+++ pages/components/userListForm.tsx
@@ -1,3 +1,5 @@
+//userListForm.tsx pm 2시부터 작업하던 것들.. 
+
 import React, { useEffect, useState } from 'react';
 import styles from '../../styles/UserList.module.css';
 import mafia_reverse from '../../Images/gun_reverse.png'; // LogoReverse
@@ -8,7 +10,12 @@ import Image from 'next/image'; // next/image 패키지 import
 import { getSocket } from '../utils/ClientSocket'; // Socket.ts 파일에서 getSocket 함수를 import
 import { getRoom } from '../utils/Room';
 
-const _userListForm: React.FC = () => {
+// 여기에 추가
+interface UserRole {
+  role: string;
+}
+
+  const _userListForm: React.FC = () => {
   const [userList, setUserList] = useState<string[]>([]);
   const socket = getSocket(); 
   const [userRole, setUserRole] = useState<string>(''); // 역할 상태 추가
@@ -21,9 +28,9 @@ const _userListForm: React.FC = () => {
       console.log(users);
     });
 
-    socket.on('roleAssignment', (role: string) => { // 역할 할당을 처리하는 부분 추가
+    socket.on('roleAssignment', (role: string) => { 
       setUserRole(role);
-    });
+    });// 역할 할당을 처리하는 부분 추가
 
     socket.on('reloadGame', (user: string) => {
       setUserList(prevUsers => prevUsers.filter(existingUser => existingUser !== user));
@@ -46,10 +53,10 @@ const _userListForm: React.FC = () => {
         <div className={styles.widgetContent}>
         <h2>My role</h2>
           <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
-          <Image src={mafia_reverse} alt="role" width={145} height={160}></Image>Mafia
-          <Image src={police_reverse} alt="role" width={145} height={160}></Image>Police
-          <Image src={doctor_reverse} alt="role" width={145} height={160}></Image>Doctor
-          <Image src={citizen} alt="role" width={145} height={160}></Image>Citizen
+          {userRole.role == 'Mafia' && <><Image src={mafia_reverse} alt="role" width={145} height={160}></Image>Mafia</>}
+          {userRole.role == 'Police' && <><Image src={police_reverse} alt="role" width={145} height={160}></Image>Police</>}
+          {userRole.role == 'Doctor' && <><Image src={doctor_reverse} alt="role" width={145} height={160}></Image>Doctor</>}
+          {userRole.role == 'Citizen' && <><Image src={citizen} alt="role" width={145} height={160}></Image>Citizen</>}
           </div>
 
         </div>
